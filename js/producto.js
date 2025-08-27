4d47aaaf878b9f84a9d8c2571b6b8403fe2e1748
LautiSmyth <lauti.smyth@gmail.com>
2025-08-26 21:22:51 -0300

    Update producto.js

--- js/producto.js
+++ js/producto.js
@@ -1,41 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function getQueryParam(param) {
+    function getQueryParam(parametro) {
         const params = new URLSearchParams(window.location.search);
-        return params.get(param);
+        return params.get(parametro);
     }
 
     const id = parseInt(getQueryParam('id'), 10);
     const cont = document.getElementById('productoDetalle');
 
-    const prod = productos.find(p => p.id === id);
+    const producto = productos.find(p => p.id === id);
 
-    if (!prod) {
+    if (!producto) {
         cont.innerHTML = '<div class="alert alert-danger">Producto no encontrado.</div>';
         return;
     }
 
-    // Generar HTML de atributos dinámicamente
     let atributosHTML = '';
-    prod.atributos.forEach(attr => {
+    producto.atributos.forEach(attr => {
         atributosHTML += `<li class="list-group-item"><strong>${attr.nombre}:</strong> ${attr.valor}</li>`;
     });
 
     cont.innerHTML = `
         <div class="row g-0">
             <div class="col-md-6">
-                <img src="${prod.imagen}" class="img-fluid rounded-start" alt="${prod.titulo}">
+                <img src="${producto.imagen}" class="img-fluid rounded-start" alt="${producto.titulo}">
             </div>
             <div class="col-md-6 d-flex flex-column justify-content-center">
                 <div class="card-body">
-                    <h3 class="card-title">${prod.titulo}</h3>
-                    <p class="card-text">${prod.texto}</p>
+                    <h3 class="card-title">${producto.titulo}</h3>
+                    <p class="card-text">${producto.texto}</p>
 
                     <h5 class="mt-4">Especificaciones</h5>
                     <ul class="list-group list-group-flush">
                         ${atributosHTML}
                     </ul>
 
-                    <button class="btn btn-primary w-100 mt-4" onclick="agregarAlCarrito(${prod.id})">Añadir al Carrito</button>
+                    <button class="btn btn-primary w-100 mt-4" onclick="agregarAlCarrito(${producto.id})">Añadir al Carrito</button>
                 </div>
             </div>
         </div>
