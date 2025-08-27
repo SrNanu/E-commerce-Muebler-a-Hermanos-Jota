988a866ed9b6af3e63cf357819b182c12d1099b0
LautiSmyth <lauti.smyth@gmail.com>
2025-08-27 01:05:09 -0300

    Footer y Header
    creacion de footer y header general para todas las paginas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let carrito = [];
 
+function cargarHeaderFooter() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('header').innerHTML = data);
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('footer').innerHTML = data);
+}
+
 function cargarCarrito() {
     try {
         const raw = localStorage.getItem('carrito');
