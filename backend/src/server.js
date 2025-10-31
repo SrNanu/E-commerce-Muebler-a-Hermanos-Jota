30254ec104cbb051434648b7292e0aab7aaad914
LautiSmyth <lauti.smyth@gmail.com>
2025-10-31 15:06:02 -0300

    Configuracion de MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const app = require('./app'); 
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 4000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
